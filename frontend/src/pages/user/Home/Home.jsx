619f1cb086628ba639c8029bdce54c0c061718e2
Amen <edohadevelops@gmail.com>
2024-07-24 01:02:03 +0100

    Done for the night

--- frontend/src/pages/user/Home/Home.jsx
+++ frontend/src/pages/user/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
 import * as routes from '../../../routes/routes';
 import Avatar from '../../../assets/Avatar.png';
-import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
+// import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
 import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
 import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 import SportsSoccerOutlinedIcon from '@mui/icons-material/SportsSoccerOutlined';
@@ -13,10 +13,12 @@ import './style.css';
 import Card from '../../../components/cards/Card';
 import { AppContext } from '../../../store/AppContext'
 import { axiosQuery } from '../../../utils/api';
+import CampaignModal from '../../../components/modal/campaign/CampaignModal'
 
 const Home = () => {
 
     const {userDetails,allCampaigns,setAllCampaigns} = useContext(AppContext);
+    const [postOpen,setPostOpen] = useState(false)
     const getCampaigns = () => {
         console.log("About to make request")
         axiosQuery.get("http://localhost:5000/api/campaign/getAll")
@@ -31,73 +33,79 @@ const Home = () => {
         getCampaigns();
     },[])
     return (
-        <div className="home">
-            <div className="intro-section">
-                <div className="intro-details">
-                    <p className="page-title">Hi, {userDetails?.first_name}</p>
-                    <p className="intro-description">Let's start spreading Greatness</p>
+        <>
+            <div className="home">
+                <div className="intro-section">
+                    <div className="intro-details">
+                        <p className="page-title">Hi, {userDetails?.first_name}</p>
+                        <p className="intro-description">Let's start spreading Greatness</p>
+                    </div>
+                    <div className="intro-avatar">
+                        <Link className='' to={routes.NOTIFICATIONS}>
+                            <NotificationsActiveOutlinedIcon className='notification-icon' />
+                        </Link>
+                        <img src={Avatar} alt="avatar.jpg" onClick={()=>setPostOpen(true)} />
+                    </div>
                 </div>
-                <div className="intro-avatar">
-                    <Link className='' to={routes.NOTIFICATIONS}>
-                        <NotificationsActiveOutlinedIcon className='notification-icon' />
-                    </Link>
-                    <img src={Avatar} alt="avatar.jpg" />
+                <div className="filter-section">
+                    <input type='text' placeholder="Seach for anything" />
+                    <div className="category-filter">
+                        All Categories
+                    </div>
+                </div>
+                <div className="category-section">
+                    <p className="category-title">Categories</p>
+                    <div className="category-links">
+                        <div className="category-link">
+                            <div className="category-icon-div">
+                                <AccountBalanceOutlinedIcon className="category-icon" />
+                            </div>
+                            <p>Medical</p>
+                        </div>
+                        <div className="category-link">
+                            <div className="category-icon-div">
+                                <AutoStoriesOutlinedIcon className="category-icon" />
+                            </div>
+                            <p>Education</p>
+                        </div>
+                        <div className="category-link">
+                            <div className="category-icon-div">
+                                <BusinessCenterOutlinedIcon className="category-icon" />
+                            </div>
+                            
+                            <p>Business</p>
+                        </div>
+                        <div className="category-link">
+                            <div className="category-icon-div">
+                                <SportsSoccerOutlinedIcon className="category-icon" />
+                            </div>
+                            <p>Sports</p>
+                        </div>
+                        <div className="category-link">
+                            <div className="category-icon-div">
+                                <MoreHorizOutlinedIcon className="category-icon" />
+                            </div>
+                            
+                            <p>See More</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="trending-section">
+                    <p className="trending-title">Trending Campaigns</p>
+                    <div className="trending-results">
+                        {
+                            allCampaigns?.map((item,index)=>(
+                                <Card details={item} index={index} />
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
-            <div className="filter-section">
-                <input type='text' placeholder="Seach for anything" />
-                <div className="category-filter">
-                    All Categories
-                </div>
-            </div>
-            <div className="category-section">
-                <p className="category-title">Categories</p>
-                <div className="category-links">
-                    <div className="category-link">
-                        <div className="category-icon-div">
-                            <AccountBalanceOutlinedIcon className="category-icon" />
-                        </div>
-                        <p>Medical</p>
-                    </div>
-                    <div className="category-link">
-                        <div className="category-icon-div">
-                            <AutoStoriesOutlinedIcon className="category-icon" />
-                        </div>
-                        <p>Education</p>
-                    </div>
-                    <div className="category-link">
-                        <div className="category-icon-div">
-                            <BusinessCenterOutlinedIcon className="category-icon" />
-                        </div>
-                        
-                        <p>Business</p>
-                    </div>
-                    <div className="category-link">
-                        <div className="category-icon-div">
-                            <SportsSoccerOutlinedIcon className="category-icon" />
-                        </div>
-                        <p>Sports</p>
-                    </div>
-                    <div className="category-link">
-                        <div className="category-icon-div">
-                            <MoreHorizOutlinedIcon className="category-icon" />
-                        </div>
-                        
-                        <p>See More</p>
-                    </div>
-                </div>
-            </div>
-            <div className="trending-section">
-                <p className="trending-title">Trending Campaigns</p>
-                <div className="trending-results">
-                    {
-                        allCampaigns?.map((item,index)=>(
-                            <Card details={item} index={index} />
-                        ))
-                    }
-                </div>
-            </div>
-        </div>
+            {
+                postOpen &&
+                <CampaignModal setModal={setPostOpen} />
+            }
+        </>
     )
 }
 
