de1ea99f69ba8ccbec6bbc88b82990f9b14ca669
Amen <edohadevelops@gmail.com>
2024-08-01 02:52:59 +0100

    Going to bed

--- backend/controllers/donation/webhook.js
+++ backend/controllers/donation/webhook.js
@@ -7,26 +7,31 @@ export default (req,res) => {
     const secret = process.env.PAYSTACK_SECRET
     const hash = createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
 
+    console.log("Event came through")
+
+
     if(hash === req.headers['x-paystack-signature']){
         const event = req.body;
         res.send(200)
         console.log("Event from paystack is: ", event);
-        models.Donation.update(
-            {
-                donation_status: event.data.status
-            },
-            {
-                where: {
-                    donation_id: event.data.metadata.donation_id
+        if(event.event === "charge.success"  || event.event === "charge.failed" ){
+            models.Donation.update(
+                {
+                    donation_status: event.data.status
+                },
+                {
+                    where: {
+                        donation_id: event.data.metadata.donation_id
+                    }
                 }
-            }
-        )
-        .then((data)=>{
-            console.log("Donation updated successfully",data)
-        })
-        .catch((err)=>{
-            console.log("Error occured while updating donation: ",err)
-        })
+            )
+            .then((data)=>{
+                console.log("Donation updated successfully",data)
+            })
+            .catch((err)=>{
+                console.log("Error occured while updating donation: ",err)
+            })
+        }
 
         
     }
