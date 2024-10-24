c0f735f498eb3818a5f028ee54bbd351a8a605ce
Amen <edohadevelops@gmail.com>
2024-10-24 14:45:59 +0100

    Updated the database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,15 @@
 import { Sequelize,DataTypes } from 'sequelize';
 
 
-const sequelize = new Sequelize('fundrise','root','',{
-  host: 'localhost',
-  dialect: 'mysql'
+// const sequelize = new Sequelize('fundrise','root','',{
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
+
+const sequelize = new Sequelize('sql7740398','sql7740398','hMWVcvzF9y',{
+  host: 'sql7.freesqldatabase.com',
+  dialect: 'mysql',
+  port: 3306
 });
 
 export const connectToDB = () => {
